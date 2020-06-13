1fa36c27a162accbce17fc8d1513c2165ad2e2cb
Yuri Duarte <yuriduartedev@gmail.com>
2020-06-13 04:13:43 -0300

    Finaliza exercício 18

--- 17/app.js
+++ 17/app.js
@@ -1,27 +1,25 @@
 const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', function() {
-  const input1 = Number(document.querySelector('#soma1').value);
-  const input2 = Number(document.querySelector('#soma2').value);
+  const input1 = document.querySelector('#input1').value;
+  const input2 = document.querySelector('#input2').value;
   const span = document.querySelector('span');
 
-  const result = sum(input1, input2);
+  const result = rest(input1, input2);
   show(result);
 
-  // Comentário de uma linha
-  function sum(n1, n2) {
-    return n1 + n2;
+  function rest(n1, n2) {
+    return n1 % n2;
   }
 
-  function show(result) {
+  function show() {
     clear();
     span.appendChild(document.createTextNode(`Resultado: ${result}`))
   }
 
   function clear() {
-    span.textContent= '';
-    document.querySelector('#soma1').value='';
-    document.querySelector('#soma2').value='';
+    span.textContent = '';
+    document.querySelector('#input1').value = '';
+    document.querySelector('#input2').value = '';
   }
-
 });
