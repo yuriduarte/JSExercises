6f160faa8b3ac93c83bd903d0b25b4225f5f4c30
Yuri Duarte <yuriduartedev@gmail.com>
2020-06-13 03:39:32 -0300

    Finaliza exercício 10

--- 10/app.js
+++ 10/app.js
@@ -1,5 +1,26 @@
-const btn = document.querySelector('btn');
+const btn = document.querySelector('#btn');
 
 btn.addEventListener('click', function() {
-  console.log('Clicou');
-})
+  const input1 = Number(document.querySelector('#soma1').value);
+  const input2 = Number(document.querySelector('#soma2').value);
+  const span = document.querySelector('span');
+
+  const result = sum(input1, input2);
+  show(result);
+
+  function sum(n1, n2) {
+    return n1 + n2;
+  }
+
+  function show(result) {
+    clear();
+    span.appendChild(document.createTextNode(`Resultado: ${result}`))
+  }
+
+  function clear() {
+    span.textContent= '';
+    document.querySelector('#soma1').value='';
+    document.querySelector('#soma2').value='';
+  }
+
+});
