ae613cae85060c23f3b52f047d160042e2cf646c
Yuri Duarte <yuriduartedev@gmail.com>
2020-06-15 18:45:25 -0300

    Finaliza exercício 31

---
+++ 31/app.js
@@ -0,0 +1,31 @@
+function start() {
+  const span = document.querySelector('span');
+
+  let operator = 0;
+  for (let iE = 0; iE < 11; iE++) {
+    span.appendChild(document.createElement('p')).innerText = `Tabuada de: ${iE}`
+
+    for (let i = 0; i < 10; i++) {
+      const p = document.createElement('p');
+      const index = i + 1;
+      // console.log(index);
+
+
+      function calc(operator, index) {
+        return operator * index;
+      }
+
+      const result = calc(operator, index);
+
+      span.appendChild(p).innerHTML = `${operator} x ${index} = ${result}` ;
+    }
+
+    span.appendChild(document.createElement('br'));
+
+    operator = operator + 1;
+    console.log(operator);
+  }
+
+}
+
+start();
